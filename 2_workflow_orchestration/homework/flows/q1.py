37d82de541e2f2ada68f343d382eb09246e3b243
Gerald Ooi <27208744+geraldooi@users.noreply.github.com>
2023-02-03 09:21:27 +0800

    Add week 2 - homework question 1

---
+++ 2_workflow_orchestration/homework/flows/q1.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from pathlib import Path
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data from web into pandas DataFrame"""
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write dataframe our locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task(log_prints=True)
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to gcs"""
+    gcp_cloud_storage_bucket_block = GcsBucket.load("prefect-de-zoomcamp-375108")
+    gcp_cloud_storage_bucket_block.upload_from_path(
+        from_path=path,
+        to_path=path
+    )
+
+
+@flow()
+def etl_web_to_gcs(color: str = "green", year: int = 2020, month: int = 1) -> None:
+    """The main ETL function"""
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+
+if __name__ == "__main__":
+    color = "green"
+    year = 2020
+    month = 1
+    etl_web_to_gcs(color, year, month)
