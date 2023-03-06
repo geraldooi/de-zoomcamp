b176a373149cce184d8ab0d70cb730e5cccadbd4
Gerald Ooi <27208744+geraldooi@users.noreply.github.com>
2023-03-06 19:41:55 +0800

    Comment the limit statement in staging tables

--- 4_analytics_engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 4_analytics_engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -10,8 +10,8 @@ select
     affiliated_base_number
 from {{ source("staging", "fhv_trip_external") }}
 
-{% if var('is_test_run', default=true) %}
+{# {% if var('is_test_run', default=true) %}
 
     limit 100
 
-{% endif %}
+{% endif %} #}
